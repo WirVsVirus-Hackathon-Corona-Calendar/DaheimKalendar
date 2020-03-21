52eafa800a6ff90a2d0abfcf16478542f558525f
garritfra <garritfranke@gmail.com>
2020-03-21 02:00:44 +0100

    Add simple network call

--- app/src/main/java/de/garritfra/daheimkalender/MainActivity.java
+++ app/src/main/java/de/garritfra/daheimkalender/MainActivity.java
@@ -3,6 +3,19 @@ package de.garritfra.daheimkalender;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("https://e3bzj7x3ck.execute-api.eu-west-1.amazonaws.com/v1/challenges")
+                        .build();
+
+                Call call = client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        Log.e("E", String.valueOf(e.getStackTrace()));
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        Log.d("OKHTTP", response.body().string());
+                    }
+                });
+
+
+            }
+        }).run();
     }
+
+
 }
