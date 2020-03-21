378bc97786b76a07dc963968e6c353f1907382d4
englich <englich@ckc.de>
2020-03-21 02:20:34 +0100

    * Added new Activity to present the graphic novel * Added Graphic Novel Challenge Fragment * Added Graphic Novel Challenge Done Fragment

---
+++ app/src/main/java/de/garritfra/daheimkalender/ui/graphicnovel/GraphicNovelActivity.java
@@ -0,0 +1,26 @@
+package de.garritfra.daheimkalender.ui.graphicnovel;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import de.garritfra.daheimkalender.R;
+
+public class GraphicNovelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphic_novel);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
---
+++ app/src/main/java/de/garritfra/daheimkalender/ui/graphicnovel/GraphicNovelChallengeFragment.java
@@ -0,0 +1,36 @@
+package de.garritfra.daheimkalender.ui.graphicnovel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import de.garritfra.daheimkalender.R;
+
+public class GraphicNovelChallengeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_graphic_novel_challenge, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(GraphicNovelChallengeFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+}
