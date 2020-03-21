bfb51f61e8f123ada5c2b01827a706d19324c49e
JanBdot <Stars13poker>
2020-03-21 01:29:26 +0100

    Fragment class erstellt für today challenge

--- app/src/main/java/de/garritfra/daheimkalender/MainActivity.java
+++ app/src/main/java/de/garritfra/daheimkalender/MainActivity.java
@@ -1,6 +1,8 @@
 package de.garritfra.daheimkalender;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        TodayChallengeFragment fragment = new TodayChallengeFragment();
+        fragmentTransaction.add(R.id.frame, fragment);
+        fragmentTransaction.commit();;
     }
 }
