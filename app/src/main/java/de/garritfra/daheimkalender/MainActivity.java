b4e45d3578ae7356e20ab57b4f236f0b94baafdd
Your Name <you@example.com>
2020-03-21 01:59:32 +0100

    add fragment challenge_tagebuch

--- app/src/main/java/de/garritfra/daheimkalender/MainActivity.java
+++ app/src/main/java/de/garritfra/daheimkalender/MainActivity.java
@@ -1,14 +1,31 @@
 package de.garritfra.daheimkalender;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import de.garritfra.daheimkalender.ui.ChallengeTagebuchFragment;
+import de.garritfra.daheimkalender.ui.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements ChallengeTagebuchFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        ChallengeTagebuchFragment fragment = new ChallengeTagebuchFragment();
+        fragmentTransaction.add(R.id.frame, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.Challenge item) {
+
     }
 }
